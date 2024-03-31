172b1ad699ec563ba3da0b322b424327aa82d1df
Ethan Hao <hao.yz.ethan@gmail.com>
2024-03-31 11:42:57 -0400

    build navbar

---
+++ app/NavBar.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import {AiFillBug} from "react-icons/ai";
+
+const NavBar = () => {
+    const links = [
+        {href: "/", label: "Dashboard"},
+        {href: "/issues", label: "Issues"},
+    ];
+
+    return (
+        <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
+            <Link href="/"><AiFillBug/></Link>
+            <ul className="flex space-x-6">
+                {links.map(link =>
+                    <li key={link.href}>
+                        <Link
+                            href={link.href}
+                            className="text-zinc-500 hover:text-zinc-800 transition-colors"
+                        >
+                            {link.label}
+                        </Link>
+                    </li>)}
+            </ul>
+        </nav>
+
+    );
+};
+
+export default NavBar;
