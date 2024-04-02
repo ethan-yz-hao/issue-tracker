78b1c7410be4b1d64722422560f5b4b832780348
Ethan Hao <hao.yz.ethan@gmail.com>
2024-04-01 22:50:13 -0400

    add loading effect for deleting issue

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -3,16 +3,20 @@ import {AlertDialog, Button, Flex} from "@radix-ui/themes";
 import axios from "axios";
 import {useRouter} from "next/navigation";
 import {useState} from "react";
+import {Spinner} from "@/app/components";
 
 const DeleteIssueButton = ({issueId}: { issueId: number }) => {
     const router = useRouter();
     const [error, setError] = useState(false);
+    const [isDeleting, setIsDeleting] = useState(false);
     const handleDelete = async () => {
         try {
+            setIsDeleting(true);
             await axios.delete(`/api/issues/${issueId}`);
             router.push('/issues');
             router.refresh();
         } catch (error) {
+            setIsDeleting(false);
             setError(true);
         }
     };
@@ -21,7 +25,10 @@ const DeleteIssueButton = ({issueId}: { issueId: number }) => {
         <>
             <AlertDialog.Root>
                 <AlertDialog.Trigger>
-                    <Button color="red">Delete Issue</Button>
+                    <Button color="red" disabled={isDeleting}>
+                        Delete Issue
+                        {isDeleting && <Spinner />}
+                    </Button>
                 </AlertDialog.Trigger>
                 <AlertDialog.Content>
                     <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
