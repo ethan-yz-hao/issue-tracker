1518c0b3f6331a293b78debb9f162dc56e4006b3
Ethan Hao <hao.yz.ethan@gmail.com>
2024-04-02 17:40:19 -0400

    setup assignee select component

---
+++ app/api/users/route.ts
@@ -0,0 +1,14 @@
+import {NextRequest, NextResponse} from "next/server";
+import prisma from "@/prisma/client";
+import {getServerSession} from "next-auth";
+import authOptions from "@/app/auth/authOptions";
+
+export async function GET(request: NextRequest) {
+    const session = await getServerSession(authOptions);
+    if (!session) {
+        return NextResponse.json({error: "Unauthorized"}, {status: 401});
+    }
+
+    const users = await prisma.user.findMany({orderBy: {name:'asc'}})
+    return NextResponse.json(users, {status: 200});
+}
