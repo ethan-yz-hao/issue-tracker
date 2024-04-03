ee3dd4a1979f6627bc596b52cde65839579f9837
Ethan Hao <hao.yz.ethan@gmail.com>
2024-04-02 23:16:12 -0400

    build pagination component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
+import Pagination from "@/app/components/Pagination";
+
 export default function Home() {
   return (
-    <div>Hello World</div>
+    <Pagination itemCount={100} pageSize={10} currentPage={2}/>
   );
 }
